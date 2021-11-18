cce795c08a34cb332f7ec044e25e0a14a9f20f93
topotp <48508401+Otakuprimchoco@users.noreply.github.com>
2021-11-18 23:39:44 +1100

    create event button

---
+++ src/app/assets/buttons/SubButton1.js
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  ScrollView, StyleSheet,
+  Text, TouchableOpacity, View
+} from "react-native";
+import { Header, Icon } from 'react-native-elements';
+
+export default function SubButton1({text, onPressFn, icon, color}) {
+  const styles = StyleSheet.create( {
+    button: {
+      width: 160,
+      height: 35,
+      backgroundColor: color,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 5, 
+      flexDirection: 'row'
+    },
+    buttonText: {
+      fontSize: 18,
+      fontFamily: 'Roboto',
+      fontWeight: 'bold',
+      color: 'white',
+      // paddingHorizontal: 10,
+      // paddingRight: 5,
+      // width: 100,
+      alignSelf: 'center'
+    },
+    buttonContainer: {
+      // paddingHorizontal: 5,
+      flex: 1
+    },
+    iconContainer: {
+      paddingHorizontal: 5
+    }
+  });  
+  
+  return (
+    <TouchableOpacity onPress={onPressFn}>
+      <View style={styles.button} >
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>{text}</Text>
+        </View>
+        <View style={styles.iconContainer}>
+          <Icon name={icon} color='white' size={25}/>
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
--- src/app/screens/events/MainScreens/DiscoverEvents.js
+++ src/app/screens/events/MainScreens/DiscoverEvents.js
@@ -7,7 +7,8 @@ import {
   TouchableOpacity} from "react-native";
 import EventCard from "../../../assets/Cards/EventCard/EventCard";
 import SearchAndFilter from "../../../assets/SearchAndFilter/SearchAndFilter";
-import discoveryData from '../../../assets/sampleUserData/Sample_Event_Data/sample_discover_events_data'
+import discoveryData from '../../../assets/sampleUserData/Sample_Event_Data/sample_discover_events_data';
+import SubButton1 from "../../../assets/buttons/SubButton1";
 
 export default function DiscoverEvents({navigation}) {
   const EventData = discoveryData[1]
@@ -15,6 +16,9 @@ export default function DiscoverEvents({navigation}) {
   return (
     <View style={styles.container}>
         <View style={styles.eventsContainer}>
+          <View style={styles.followButtonContainer}>
+            <SubButton1 text={"Create Event"} color={styles.followButton.backgroundColor} icon={'add-circle'} onPressFn={() => {navigation.navigate("CreateGroup1")}} />
+          </View>
           <View style={{paddingBottom: 5, paddingLeft: 5}}>
             <SearchAndFilter/>
           </View>
@@ -59,5 +63,12 @@ const styles = StyleSheet.create({
     borderColor: 'lightgrey',
     borderWidth: 1,
     backgroundColor: '#F2F2F3'
+  },
+  followButtonContainer: {
+    alignItems: 'center',
+    marginTop: 10
+  },
+  followButton: {
+    backgroundColor: '#66B2B2',
   },
   });
